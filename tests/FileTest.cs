9b0f0976e51d81a0770818031ed0620543dc86e9
YoshinoriN <nayo-msoft@outlook.jp>
2020-12-30 23:58:01 +0900

    feat: implement ReadFile function

---
+++ tests/FileTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using System.IO;
+
+namespace SiteStatus.Tests
+{
+    public class FileTest
+    {
+        [Fact]
+        public void FileReadableTest()
+        {
+            var content = SiteStatus.utils.File.ReadFile(Path.Join(Directory.GetCurrentDirectory(), "settings.example.json"));
+            Assert.StartsWith("{", content);
+        }
+
+        [Fact]
+        public void ThrowFileNotFoundExceptionTest()
+        {
+            Assert.Throws<FileNotFoundException>(() =>
+            {
+                SiteStatus.utils.File.ReadFile(Path.Join(Directory.GetCurrentDirectory(), "notfound.example.settings.json"));
+            });
+        }
+    }
+}
