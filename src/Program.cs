17caa164b4fe5fbec9b67467e27a44de59169830
YoshinoriN <nayo-msoft@outlook.jp>
2020-12-30 22:04:43 +0900

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiteStatus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
