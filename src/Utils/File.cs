dfa68ea798685ae05cb4dd9452846eefda703567
YoshinoriN <nayo-msoft@outlook.jp>
2020-12-31 22:45:17 +0900

    fix: direcroty name lowercase to uppercase

---
+++ src/Utils/File.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace SiteStatus.utils
+{
+    public static class File
+    {
+        public static string ReadFile(string path)
+        {
+            using (var streamReader = new StreamReader(path))
+            {
+                return streamReader.ReadToEnd();
+            };
+        }
+    }
+}
