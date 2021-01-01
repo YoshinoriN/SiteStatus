9436e315eecd9f1d80f5475a3d443bd6bd408194
YoshinoriN <nayo-msoft@outlook.jp>
2021-01-01 22:08:20 +0900

    feat: implement whois

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,9 @@ using System.IO;
 using SiteStatus.Domains.Services;
 using SiteStatus.Infrastructures.Settings;
 using SiteStatus.Utils;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Whois.Models;
 
 namespace SiteStatus
 {
@@ -19,6 +22,21 @@ namespace SiteStatus
             // TODO: exec parallelly
             foreach (string domain in settings.Domains)
             {
+                WhoisResponse whoisResult = null;
+                try
+                {
+                    whoisResult = SiteStatus.Utils.Whois.Lookup(domain)
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception ex)
+                {
+                    // TODO: logging
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                Console.WriteLine(JsonSerializer.Serialize(whoisResult.ParsedResponse));
+
                 ServerCertificate result = null;
                 try
                 {
---
+++ src/Utils/Whois.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Whois;
+using Whois.Models;
+
+namespace SiteStatus.Utils
+{
+    public static class Whois
+    {
+        public static async Task<WhoisResponse> Lookup(string domain)
+        {
+            var whois = new WhoisLookup();
+            return await whois.LookupAsync(domain);
+        }
+    }
+}
